8a24f02fad631607dddb9c950720ad36c124cf14
Flawmaster <flawmaster@yandex.ru>
2020-03-21 15:10:52 +0300

    Modified def name - test_add_to_basket_button

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def testAddToBasketButton(browser):
+def test_add_to_basket_button(browser):
 	browser.get(link)
 	time.sleep(30)
 	#True, если будет любое значение у метода *.find_element(), кроме None
